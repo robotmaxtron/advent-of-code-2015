854f5499680f7a85a5663b774ce4a5c5231e03b2
Max Whittingham <max.whittingham@gmail.com>
2020-09-18 14:01:43 -0600

    Day 04, pt1&2

---
+++ day-04/day1-pt1.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"regexp"
+)
+
+func main() {
+	data, err := ioutil.ReadFile("day4-input.txt")
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return
+	}
+	number := 0
+	regex := regexp.MustCompile(`\A00000`)
+	digest := ""
+
+	for !regex.MatchString(digest) {
+		number++
+		hash := md5.New()
+		io.WriteString(hash, string(data))
+		io.WriteString(hash, fmt.Sprintf("%d", number))
+		digest = fmt.Sprintf("%x", hash.Sum(nil))
+	}
+	fmt.Println(number)
+}
